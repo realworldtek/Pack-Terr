4917feee090ab919b9ea03ddeec4b112c2db2cd5
jenkins <kwalar.nformi@gmail.com>
2018-04-22 15:39:25 -0400

    modified

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo echo "This is a CICD deployment" > /usr/share/nginx/html/index.html && sudo service nginx restart
+sudo echo "This is a CICD deployment for staging website" > /usr/share/nginx/html/index.html && sudo systemctl restart nginx
 
